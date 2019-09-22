18a08a38ee173ed6226fd5adfb5eb01048184fbd
venkat prudhvi Dommaraju <S537236@nwmissouri.edu>
2019-09-22 12:46:08 -0500

    modified

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ function calculate(){
     const tip=parseFloat(total)*rate/100;
     const tax=parseFloat(total)*5.5/100;
     const grandtotal=parseFloat(total)+parseFloat(rate)+tax;
-    document.getElementById("tip").innerHTML=parseFloat(tip).toFixed(2);
-    document.getElementById("tax").innerHTML=parseFloat(tax).toFixed(2);
-    document.getElementById("grandtotal").innerHTML=parseFloat(grandtotal).toFixed(2);
+    document.getElementById("tip").innerHTML="$"+parseFloat(tip).toFixed(2);
+    document.getElementById("tax").innerHTML="$"+parseFloat(tax).toFixed(2);
+    document.getElementById("grandtotal").innerHTML="$"+parseFloat(grandtotal).toFixed(2);
 }
